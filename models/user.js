6f12f56a91cc5d06a97dda1cc00ea871395519a2
aadilakshmi gonuguntla <aadilakshmigonuguntla@gmail.com>
2026-09-04 10:43:50 +0530

    sequlize in nodejs

---
+++ models/user.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      comment: "Here to store the name of the user",
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      set(value) {
+        if (value) {
+          this.setDataValue('email', value.toLowerCase());
+        }
+      }
+    },
+    gender: {
+      type: DataTypes.ENUM(['male', 'female']),
+      allowNull: false,
+      defaultValue: 'female'
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
